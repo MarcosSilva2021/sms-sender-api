0d8df743d7c73485f63601d94365b4fec2423707
Marcosdsj <mfsilva5585@gmail.com>
2025-03-19 21:28:19 -0300

    Initial commit

---
+++ sms-sender-api/src/main/java/dev/mar/sms/SmsSenderApiApplication.java
@@ -0,0 +1,13 @@
+package dev.mar.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsSenderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsSenderApiApplication.class, args);
+	}
+
+}
